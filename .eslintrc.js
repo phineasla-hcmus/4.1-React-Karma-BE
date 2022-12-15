faab554217b64e1f269860270953941f3ec148fc
phineasla <63248715+phineasla@users.noreply.github.com>
2022-12-16 00:34:11 +0700

    Enable ESLint import ordering

--- .eslintrc.js
+++ .eslintrc.js
@@ -23,5 +23,15 @@ module.exports = {
     '@typescript-eslint/explicit-function-return-type': 'off',
     '@typescript-eslint/explicit-module-boundary-types': 'off',
     '@typescript-eslint/no-explicit-any': 'off',
+    'import/order': [
+      'error',
+      {
+        alphabetize: {
+          order: 'asc',
+          caseInsensitive: false,
+        },
+        'newlines-between': 'always',
+      },
+    ],
   },
 };
