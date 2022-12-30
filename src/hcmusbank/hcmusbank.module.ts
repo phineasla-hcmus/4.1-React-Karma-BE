dc1689b04b0a0da6a11ef08becfc71f5e8080b25
phineasla <63248715+phineasla@users.noreply.github.com>
2022-12-30 22:21:18 +0700

    Add hcmusbank findOneAccount

--- src/hcmusbank/hcmusbank.module.ts
+++ src/hcmusbank/hcmusbank.module.ts
@@ -1,10 +1,12 @@
+import { HttpModule } from '@nestjs/axios';
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 
 import { HcmusbankService } from './hcmusbank.service';
 
 @Module({
   providers: [HcmusbankService],
-  imports: [],
+  imports: [ConfigModule, HttpModule],
   exports: [HcmusbankService],
 })
 export class InterbankModule {}
--- src/hcmusbank/hcmusbank.service.ts
+++ src/hcmusbank/hcmusbank.service.ts
@@ -1,6 +1,37 @@
+import { publicEncrypt } from 'crypto';
+import { readFile } from 'fs/promises';
+
+import { HttpService } from '@nestjs/axios';
 import { Injectable } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+import { catchError, firstValueFrom } from 'rxjs';
 
 @Injectable()
 export class HcmusbankService {
-  
+  baseUrl: string;
+
+  constructor(
+    private readonly configService: ConfigService,
+    private readonly httpService: HttpService,
+  ) {
+    this.baseUrl = 'https://hcmus-internet-banking-backend.vercel.app';
+  }
+
+  private async sign(payload: string) {
+    const publicKey = await readFile('hcmusbank.public.pem');
+    const encrypted = publicEncrypt(
+      { key: publicKey, oaepHash: 'SHA256' },
+      Buffer.from(payload),
+    );
+    return encrypted.toString('base64');
+  }
+
+  public async findOneAccount(id: string) {
+    const payload = JSON.stringify({ accountNumber: '1234567890' });
+    const res = await firstValueFrom(
+      this.httpService.post(`${this.baseUrl}/api/external/query-bank-number`, {
+        payload,
+      }),
+    );
+  }
 }
