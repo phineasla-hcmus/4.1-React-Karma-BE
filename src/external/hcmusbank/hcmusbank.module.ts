98d6d6ee14328c135bd17d00c2dd1257faacbc80
phineasla <63248715+phineasla@users.noreply.github.com>
2023-01-05 23:07:09 +0700

    Inject private key instead of reading file every request

--- src/external/hcmusbank/hcmusbank.module.ts
+++ src/external/hcmusbank/hcmusbank.module.ts
@@ -1,12 +1,29 @@
+import { readFile } from 'fs/promises';
+import { join } from 'path';
+
 import { Module } from '@nestjs/common';
-import { ConfigModule } from '@nestjs/config';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { async } from 'rxjs';
 
 import { AxiosModule } from '../../axios/axios.module';
 
 import { HcmusbankService } from './hcmusbank.service';
 
 @Module({
-  providers: [HcmusbankService],
+  providers: [
+    {
+      provide: 'HCMUS_PRIVATE_KEY_TOKEN',
+      inject: [ConfigService],
+      useFactory: async (configService: ConfigService) => {
+        try {
+          return await readFile(join(__dirname, 'hcmusbank.private.pem'));
+        } catch {
+          return configService.getOrThrow('HCMUSBANK_PRIVATE_KEY');
+        }
+      },
+    },
+    HcmusbankService,
+  ],
   imports: [ConfigModule, AxiosModule],
   exports: [HcmusbankService],
 })
