9d3f82657fc7cfa993a337ad493973826b77b0dd
phineasla <63248715+phineasla@users.noreply.github.com>
2023-01-07 14:40:25 +0700

    Fix balance not increase/decrease after local transfer

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,13 +1,13 @@
 import { Controller, Post, Body } from '@nestjs/common';
-import { TransferDto } from './dto/transfer.dto';
 
+import { TransferDto } from './dto/transfer.dto';
 import { UserService } from './user.service';
 
 @Controller('user')
 export class UserController {
   constructor(private readonly userService: UserService) {}
 
-  @Post()
+  @Post('transfer')
   async transfer(@Body() transferDto: TransferDto) {
     try {
       return this.userService.transfer(transferDto);
