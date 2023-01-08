4dbce40be1f4c69407edf6e68957c30b61669d9f
Nguyễn Thu Thảo Châu <thaochau2201@gmail.com>
2023-01-08 15:23:59 +0700

    Update at.guard.ts

--- src/common/guards/at.guard.ts
+++ src/common/guards/at.guard.ts
@@ -1,6 +1,7 @@
 import { ExecutionContext, Injectable } from '@nestjs/common';
 import { Reflector } from '@nestjs/core';
 import { AuthGuard } from '@nestjs/passport';
+import { Observable } from 'rxjs';
 
 @Injectable()
 export class AtGuard extends AuthGuard('jwt') {
@@ -8,7 +9,9 @@ export class AtGuard extends AuthGuard('jwt') {
     super();
   }
 
-  canActivate(context: ExecutionContext) {
+  canActivate(
+    context: ExecutionContext,
+  ): boolean | Promise<boolean> | Observable<boolean> {
     const isPublic = this.reflector.getAllAndOverride('isPublic', [
       context.getHandler(),
       context.getClass(),
