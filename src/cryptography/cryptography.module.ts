8c8f2c528a990cb5fb833e01733103a35ba7b21e
phineasla <63248715+phineasla@users.noreply.github.com>
2023-01-10 22:39:44 +0700

    Update read private key

--- src/cryptography/cryptography.module.ts
+++ src/cryptography/cryptography.module.ts
@@ -1,10 +1,27 @@
+import { readFile } from 'fs/promises';
+import { join } from 'path';
+
 import { Global, Module } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
 
 import { CryptographyService } from './cryptography.service';
 
 @Global()
 @Module({
-  providers: [CryptographyService],
+  providers: [
+    {
+      provide: 'INTERBANK_PRIVATE_KEY_TOKEN',
+      inject: [ConfigService],
+      useFactory: async (configService: ConfigService) => {
+        try {
+          return await readFile(join(__dirname, 'response.private.key'));
+        } catch {
+          return configService.getOrThrow('INTERBANK_PRIVATE_KEY');
+        }
+      },
+    },
+    CryptographyService,
+  ],
   exports: [CryptographyService],
 })
 export class CryptographyModule {}
--- src/cryptography/cryptography.service.ts
+++ src/cryptography/cryptography.service.ts
@@ -1,18 +1,21 @@
 import * as crypto from 'crypto';
 import * as fs from 'fs/promises';
 
-import { Injectable } from '@nestjs/common';
+import { Inject, Injectable } from '@nestjs/common';
 
 @Injectable()
 export class CryptographyService {
-  async sign(data: string) {
-    const privatKey = await fs.readFile('secret/response.private.key', 'utf-8');
+  constructor(
+    @Inject('INTERBANK_PRIVATE_KEY_TOKEN')
+    private readonly privateKey: Buffer | string,
+  ) {}
 
+  async sign(data: string) {
     const sign = crypto.createSign('RSA-SHA256');
     sign.update(data);
     sign.end();
 
-    const signature = sign.sign(privatKey, 'base64');
+    const signature = sign.sign(this.privateKey, 'base64');
     return signature;
   }
 
